6a25289e687632e67ec9a67c41e63db1c939314c
Valerie Bailey <v@valbailey.com>
2022-01-14 22:16:23 +0000

    Re-export ImageSource type

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,5 @@ export const IMG_SOURCE = groq`
 import _augment from './augment.js';
 import _handler from './handler.js';
 export { _augment, _handler };
+
+export type { ImageSource } from './types.js';
