d37fb391ae10c7c1aed6e1e750b08156d119c9d3
Valerie Bailey <v@valbailey.com>
2022-01-14 20:58:48 +0000

    Migrate from Sharp to Squoosh

--- src/augment.ts
+++ src/augment.ts
@@ -1,15 +1,23 @@
 import type { SanityClientLike } from '@sanity/image-url/lib/types/types';
 import type { ImageSource } from './index.js';
+// @ts-ignore: missing type definitions
+import { ImagePool } from '@squoosh/lib';
 import { arToHeight } from './helpers.js';
-import { getPlaiceholder } from 'plaiceholder';
+import fetch from 'isomorphic-unfetch';
 import imageUrlBuilder from '@sanity/image-url';
 
 export default async function augment(
   source: ImageSource,
   aspects: (string | undefined)[],
 ) {
-  const entries = aspects.map(async (ar) => [ar, await getBase64(source, ar)]);
+  const imagePool: ImagePool = new ImagePool();
+
+  const entries = aspects.map(async (ar) => {
+    return [ar, await getBase64(imagePool, source, ar)];
+  });
   source.base64 = Object.fromEntries(await Promise.all(entries));
+
+  await imagePool.close();
 }
 
 let sanityClient: SanityClientLike;
@@ -26,8 +34,20 @@ export function getImageURL(source: ImageSource | string) {
   return imageUrlBuilder(sanityClient).image(source);
 }
 
-async function getBase64(source: ImageSource, ar: string | undefined) {
+async function getBase64(
+  imagePool: ImagePool,
+  source: ImageSource,
+  ar: string | undefined,
+) {
   let builder = getImageURL(source).width(64);
   if (ar) builder = builder.height(arToHeight(ar, 64));
-  return (await getPlaiceholder(builder.url(), { size: 16 })).base64;
+  const response = await fetch(builder.url());
+
+  const image = imagePool.ingestImage(await response.arrayBuffer());
+  await image.preprocess({ resize: { enabled: true, width: 16 } });
+  await image.encode({ webp: {} });
+
+  const result = await image.encodedWith.webp;
+  const buffer = Buffer.from(result.binary);
+  return `data:image/webp;base64,${buffer.toString('base64')}`;
 }
